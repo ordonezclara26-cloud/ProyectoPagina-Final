b37cdd1b802f804698420fee9c43d4846db5b9c4
Clara <tuemail@gmail.com>
2026-04-21 15:07:33 -0600

    fix ejs

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    host: process.env.MYSQLHOST || 'roundhouse.proxy.rlwy.net',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || 'TU_PASSWORD_REAL',
+    database: process.env.MYSQLDATABASE || 'railway',
+    port: process.env.MYSQLPORT || 38330
 });
 
 db.connect(err => {
