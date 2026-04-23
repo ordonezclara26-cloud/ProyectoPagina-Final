2669ce12d8a948675ef58c23737e9330017f9f9d
Clara <tuemail@gmail.com>
2026-04-23 05:44:02 -0600

    fix conexion final mysql

--- config/db.js
+++ config/db.js
@@ -1,12 +1,6 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-});
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect(err => {
     if (err) {
