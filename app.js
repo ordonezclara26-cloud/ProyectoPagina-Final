2e169839b6576382b03b5e1174a0f8b7fee8608c
Clara <tuemail@gmail.com>
2026-04-22 22:10:03 -0600

    fix mysql duplicado app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const db = require('./config/db');
+const db = require('./config/db'); // ✔ SOLO ESTO
 const session = require('express-session');
 
 
